990b05bda5cd593f65a8ec8a45c243b13cedcf38
Jamie Klassen <cklassen@pivotal.io>
2019-10-01 13:41:03 -0400

    improve topgun worker handling
    it turns out it's important to keep your local bbl state in sync with the bucket.
    
    Signed-off-by: Jamie Klassen <cklassen@pivotal.io>
    Co-authored-by: James Thomson <jthomson@pivotal.io>

--- create-topgun.sh
+++ create-topgun.sh
@@ -2,19 +2,21 @@
 
 set -e
 
-mkdir -p $(dirname $0)/bosh-topgun-bbl-state
-cd $(dirname $0)/bosh-topgun-bbl-state
-rm -rf topgun-bbl-vars vars
-gsutil cp -r gs://topgun-bbl-vars .
-mv topgun-bbl-vars vars
-lpass show --notes 'Shared-Concourse/Topgun bbl-state.json' > bbl-state.json
+rm -rf bosh-topgun-bbl-state
+gsutil cp -r gs://bosh-topgun-bbl-state/ .
+export BBL_STATE_DIR=$PWD/bosh-topgun-bbl-state
 lpass show --notes 'Shared-Concourse/Topgun GCP Key' > topgun-gcp-key.json
-export BBL_STATE_DIR=$PWD
+export BBL_IAAS=gcp
+export BBL_GCP_REGION=us-central1
 export BBL_GCP_SERVICE_ACCOUNT_KEY=$PWD/topgun-gcp-key.json
 export BBL_GCP_SERVICE_ACCOUNT_KEY_PATH=$PWD/topgun-gcp-key.json
 export BBL_GCP_PROJECT_ID=cf-concourse-production
-export BBL_GCP_ZONE=us-central1-a
-bbl plan
-cp ../create-director.sh .
-cp ../topgun.yml cloud-config/
-cp ../topgun.tf terraform/
+export BBL_GCP_ZONE=us-central1-f
+pushd bosh-topgun-bbl-state
+  bbl plan
+  cp ../create-director.sh ./
+  cp ../topgun.yml ./cloud-config/
+  cp ../topgun.tf ./terraform/
+  bbl up
+  gsutil cp -r . gs://bosh-topgun-bbl-state/
+popd
