330fd702c26432d90c7e369700dbae0eaa0ba00d
Alex Suraci <suraci.alex@gmail.com>
2020-02-21 09:42:11 -0500

    remove redirect dns records
    it's been long enough and now that IP is something completely random so
    screw it
    
    Signed-off-by: Alex Suraci <suraci.alex@gmail.com>

--- iaas/redirect-dns.tf
+++ iaas/redirect-dns.tf
@@ -2,23 +2,3 @@ resource "google_dns_managed_zone" "concourse-ci" {
   name = "concourse-ci"
   dns_name = "concourse.ci."
 }
-
-resource "google_dns_record_set" "concourse-ci-dns" {
-  name = "${google_dns_managed_zone.concourse-ci.dns_name}"
-  type = "A"
-  ttl  = 300
-
-  managed_zone = "${google_dns_managed_zone.concourse-ci.name}"
-
-  rrdatas = ["52.45.157.165", "52.0.64.244"]
-}
-
-resource "google_dns_record_set" "www-concourse-ci-dns" {
-  name = "www.${google_dns_managed_zone.concourse-ci.dns_name}"
-  type = "CNAME"
-  ttl  = 300
-
-  managed_zone = "${google_dns_managed_zone.concourse-ci.name}"
-
-  rrdatas = ["concourse.ci-e136802e.ssl.run.pivotal.io."]
-}
